ae8e4d15b804fddf81ccdca7ff59fac40a962a17
Sirius Dely <siriusdely@icloud.com>
2013-04-05 21:18:46 +0700

    Initial Commit

---
+++ SMDReader/SMDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMDAppDelegate.h
+//  SMDReader
+//
+//  Created by SiriusDely on 4/5/13.
+//  Copyright (c) 2013 SiriusDely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SMDReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SMDReader
+//
+//  Created by SiriusDely on 4/5/13.
+//  Copyright (c) 2013 SiriusDely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMDAppDelegate class]));
+  }
+}
